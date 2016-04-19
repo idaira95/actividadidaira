9deef212f2c18514043787fcd7a128b3d679b685
Idaira Isabel <Idaira Isabel>
2016-04-19 18:57:40 +0200

    proyecto con modificacion

--- Main.java
+++ Main.java
@@ -2,28 +2,30 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package dam_ed04_actividad;
-
 
 public class Main {
 
     public static void main(String[] args) {
-        CCuenta miCuenta;
+        CCuenta cuenta1;
         double saldoActual;
 
-        miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = miCuenta.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es" + saldoActual);
 
+        operativa_cuenta(cuenta1);
+    }
+
+    private static void operativa_cuenta(CCuenta cuenta1 cantidad float) {
         try {
-            miCuenta.retirar(2300);
+            cuenta1.retirar(2300);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
         try {
             System.out.println("Ingreso en cuenta");
-            miCuenta.ingresar(695);
+            cuenta1.ingresar(695);
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
